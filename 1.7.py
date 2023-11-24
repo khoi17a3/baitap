93ae08741573831246ab8744d48693d9d2066d15
Nguyễn Trọng Khôi- DHKL17a3HN <148676816+khoi17a3@users.noreply.github.com>
2023-11-24 07:15:45 +0700

    Delete 1.7.py

--- 1.7.py
+++
@@ -1,6 +0,0 @@
-def chuyen_doi_C_sang_F(do_C)
-    do_F = (do_C * 9/5) + 32
-    return do_F
-do_C = float(input("Nhập nhiệt độ C: "))
-do_F = chuyen_doi_C_sang_F(do_C)
-print(f"ơ{do_C} độ C tương đương{do_F} độ F. ")
